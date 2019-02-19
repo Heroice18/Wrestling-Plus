b2a211d5d175b9c4788767201268072436954313
M1kester59 <michaeljashford@icloud.com>
2019-02-19 10:44:13 -0700

    Added the stubbed functionality of the User class.

--- app/src/main/java/com/example/wrestlingtournament/User.java
+++ app/src/main/java/com/example/wrestlingtournament/User.java
@@ -1,4 +1,36 @@
 package com.example.wrestlingtournament;
 
 public class User {
+
+    private String _email;
+    private String _name;
+    private String _password;
+    private int _id;
+
+    public static void User() {
+
+    }
+
+    public User login() {
+        User databaseReturnedUserType = new User();
+        return databaseReturnedUserType;
+    }
+
+    public String get_email() {
+        return _email;
+    }
+
+    public void set_email(String email) {
+        _email = email;
+    }
+
+    public String get_name() {
+        return _name;
+    }
+
+    public void set_name(String name) {
+        _name = name;
+    }
+
+
 }
