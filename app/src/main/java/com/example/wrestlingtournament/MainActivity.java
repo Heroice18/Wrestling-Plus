6dd0b24c5434b38950e8b72bdba5cf66b5a7aa19
Bradley Dawson <brad11dawson@gmail.com>
2019-02-20 13:24:55 -0700

    Added a test message to say hi

--- app/src/main/java/com/example/wrestlingtournament/MainActivity.java
+++ app/src/main/java/com/example/wrestlingtournament/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.wrestlingtournament;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+//Hey /-|-\
 public class MainActivity extends AppCompatActivity {
 
     @Override
