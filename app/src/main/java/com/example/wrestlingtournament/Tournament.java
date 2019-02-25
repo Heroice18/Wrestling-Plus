84766e0ef63da2ec0ea2486ad91e9995182f115c
CJ Waisath <38671255+Waisath-CJ@users.noreply.github.com>
2019-02-25 13:19:27 -0700

    Connected the Tournament class to the MatchActivity class

--- app/src/main/java/com/example/wrestlingtournament/Tournament.java
+++ app/src/main/java/com/example/wrestlingtournament/Tournament.java
@@ -1,7 +1,9 @@
 package com.example.wrestlingtournament;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Tournament extends AppCompatActivity {
   
@@ -9,5 +11,11 @@ public class Tournament extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_tournament);
+  }
+  
+  public void viewMatch(View m) {
+    Intent send = new Intent(this, MatchActivity.class);
+    
+    startActivity(send);
   }
 }
