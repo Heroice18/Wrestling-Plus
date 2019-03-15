b64b411b4c0862897e722eff47b7813a8dd123a6
CJ Waisath <38671255+Waisath-CJ@users.noreply.github.com>
2019-03-15 14:03:02 -0600

    Made it so you can view the name of the tournament on screen.

--- app/src/main/java/com/example/wrestlingtournament/TournamentActivity.java
+++ app/src/main/java/com/example/wrestlingtournament/TournamentActivity.java
@@ -1,16 +1,40 @@
 package com.example.wrestlingtournament;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class TournamentActivity extends AppCompatActivity {
+  FirebaseFirestore db;
+  public static final String TAG = "TournamentActivity";
+  
   
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_tournament);
+    db = FirebaseFirestore.getInstance();
+    
+    final TextView tournamentName = (TextView) findViewById(R.id.tournamentName);
+    db.collection("tournaments").document("test").get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+      @Override
+      public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+        if (task.isSuccessful()) {
+          DocumentSnapshot document = task.getResult();
+          Log.d(TAG, String.valueOf(document.getData()));
+          tournamentName.setText((CharSequence) document.get("name"));
+        }
+      }
+    });
   }
   
   public void viewMatch(View m) {
@@ -18,4 +42,8 @@ public class TournamentActivity extends AppCompatActivity {
     
     startActivity(send);
   }
+  
+  
+  
+  
 }
