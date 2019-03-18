7f2cfc7bb4cf17181988cf4a5a667c06dae1d075
M1kester59 <michaeljashford@icloud.com>
2019-03-18 13:55:48 -0600

    Deleted background thread for setting up new coaches in the database. FireBase does this automatically.

--- app/src/main/java/com/example/wrestlingtournament/Login_Start.java
+++ app/src/main/java/com/example/wrestlingtournament/Login_Start.java
@@ -151,7 +151,17 @@ public class Login_Start extends AppCompatActivity{
                                     Log.d(TAG, "onSuccess: added user");
 
                                     if (type.equals("Coach")) {
-                                        new Thread(new SetupCoachDatabase(String.valueOf(email))).start();
+                                        Map<String, Object> teamsExist = new HashMap<>();
+                                        teamsExist.put("exists", true);
+                                        db.collection("user").document(String.valueOf(email))
+                                                .collection("teams").document("varsity")
+                                                .set(teamsExist);
+                                        db.collection("user").document(String.valueOf(email))
+                                                .collection("teams").document("juniorvarsity")
+                                                .set(teamsExist);
+                                        db.collection("user").document(String.valueOf(email))
+                                                .collection("teams").document("freshman")
+                                                .set(teamsExist);
                                     }
 
                                     //go to sign in page
